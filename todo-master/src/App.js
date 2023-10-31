e1f0f40809083e34191543e4edaf9eadaf30e8e5
vaideesri <81024609+vaideesri@users.noreply.github.com>
2023-10-31 11:44:07 +0530

    Add files via upload

---
+++ todo-master/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './TodoList';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>My Todo List</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
